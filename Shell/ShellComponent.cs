c88cb621f08335071454fd19a0b5640c0e125d4a
Kristian Åland <kristaal@stud.ntnu.no>
2018-03-19 14:50:38 +0100

    mathnet imported kkode redigert

--- Shell/ShellComponent.cs
+++ Shell/ShellComponent.cs
@@ -125,146 +125,156 @@ namespace Shell
 
         private Matrix<double> CreateGlobalStiffnessMatrix(List<MeshFace> faces, List<Point3d> vertices, List<Point3d> points, double E, double A, double Iy, double Iz, double J, double G)
         {
-            int gdofs = points.Count * 6;
-            Matrix<double> K_tot = DenseMatrix.OfArray(new double[gdofs, gdofs]);
 
-            foreach (Line currentLine in geometry)
-            {
-                double L = Math.Round(currentLine.Length, 6);
 
-                Point3d p1 = new Point3d(Math.Round(currentLine.From.X, 2), Math.Round(currentLine.From.Y, 2), Math.Round(currentLine.From.Z, 2));
-                Point3d p2 = new Point3d(Math.Round(currentLine.To.X, 2), Math.Round(currentLine.To.Y, 2), Math.Round(currentLine.To.Z, 2));
 
-                double alpha = 0;
 
-                double cx = (p2.X - p1.X) / L;
-                double cy = (p2.Y - p1.Y) / L;
-                double cz = (p2.Z - p1.Z) / L;
-                double c1 = Math.Cos(alpha);
-                double s1 = Math.Sin(alpha);
-                double cxz = Math.Round(Math.Sqrt(Math.Pow(cx, 2) + Math.Pow(cz, 2)), 6);
 
-                Matrix<double> gamma;
 
-                if (Math.Round(cx, 6) == 0 && Math.Round(cz, 6) == 0)
-                {
-                    gamma = Matrix<double>.Build.DenseOfArray(new double[,]
-                {
-                    {      0, cy,  0},
-                    { -cy*c1,  0, s1},
-                    {  cy*s1,  0, c1},
-                });
-                }
-                else
-                {
-                    gamma = Matrix<double>.Build.DenseOfArray(new double[,]
-                {
-                    {                     cx,       cy,                   cz},
-                    {(-cx*cy*c1 - cz*s1)/cxz,   cxz*c1,(-cy*cz*c1+cx*s1)/cxz},
-                    {   (cx*cy*s1-cz*c1)/cxz,  -cxz*s1, (cy*cz*s1+cx*c1)/cxz},
-                });
-                }
 
-                var bd = Matrix<double>.Build;
+            #region old kmatrix code
+            //int gdofs = points.Count * 6;
+            //Matrix<double> K_tot = DenseMatrix.OfArray(new double[gdofs, gdofs]);
 
-                Matrix<double> T1;
-                T1 = gamma.Append(bd.Dense(3, 9));
-                Matrix<double> T2;
-                T2 = bd.Dense(3, 3).Append(gamma);
-                T2 = T2.Append(bd.Dense(3, 6));
-                Matrix<double> T3;
-                T3 = bd.Dense(3, 6).Append(gamma);
-                T3 = T3.Append(bd.Dense(3, 3));
-                Matrix<double> T4;
-                T4 = bd.Dense(3, 9).Append(gamma);
-                Matrix<double> T;
-                T = T1.Stack(T2);
-                T = T.Stack(T3);
-                T = T.Stack(T4);
+            //foreach (Line currentLine in geometry)
+            //{
+            //    double L = Math.Round(currentLine.Length, 6);
 
-                //Matrix<double> T = SparseMatrix.OfArray(new double[,]
-                //{
-                //    { cx, cy, cz, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                //    { cx, cy, cz, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                //    { cx, cy, cz, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                //    { 0, 0, 0, cx, cy, cz, 0, 0, 0, 0, 0, 0 },
-                //    { 0, 0, 0, cx, cy, cz, 0, 0, 0, 0, 0, 0 },
-                //    { 0, 0, 0, cx, cy, cz, 0, 0, 0, 0, 0, 0 },
-                //    { 0, 0, 0, 0, 0, 0, cx, cy, cz, 0, 0, 0 },
-                //    { 0, 0, 0, 0, 0, 0, cx, cy, cz, 0, 0, 0 },
-                //    { 0, 0, 0, 0, 0, 0, cx, cy, cz, 0, 0, 0 },
-                //    { 0, 0, 0, 0, 0, 0, 0, 0, 0, cx, cy, cz },
-                //    { 0, 0, 0, 0, 0, 0, 0, 0, 0, cx, cy, cz },
-                //    { 0, 0, 0, 0, 0, 0, 0, 0, 0, cx, cy, cz },
-                //});
+            //    Point3d p1 = new Point3d(Math.Round(currentLine.From.X, 2), Math.Round(currentLine.From.Y, 2), Math.Round(currentLine.From.Z, 2));
+            //    Point3d p2 = new Point3d(Math.Round(currentLine.To.X, 2), Math.Round(currentLine.To.Y, 2), Math.Round(currentLine.To.Z, 2));
 
-                Matrix<double> T_T = T.Transpose();
+            //    double alpha = 0;
 
-                double A1 = (E * A) / (L);
+            //    double cx = (p2.X - p1.X) / L;
+            //    double cy = (p2.Y - p1.Y) / L;
+            //    double cz = (p2.Z - p1.Z) / L;
+            //    double c1 = Math.Cos(alpha);
+            //    double s1 = Math.Sin(alpha);
+            //    double cxz = Math.Round(Math.Sqrt(Math.Pow(cx, 2) + Math.Pow(cz, 2)), 6);
 
-                double kz1 = (12 * E * Iz) / (L * L * L);
-                double kz2 = (6 * E * Iz) / (L * L);
-                double kz3 = (4 * E * Iz) / L;
-                double kz4 = (2 * E * Iz) / L;
+            //    Matrix<double> gamma;
 
-                double ky1 = (12 * E * Iy) / (L * L * L);
-                double ky2 = (6 * E * Iy) / (L * L);
-                double ky3 = (4 * E * Iy) / L;
-                double ky4 = (2 * E * Iy) / L;
+            //    if (Math.Round(cx, 6) == 0 && Math.Round(cz, 6) == 0)
+            //    {
+            //        gamma = Matrix<double>.Build.DenseOfArray(new double[,]
+            //    {
+            //        {      0, cy,  0},
+            //        { -cy*c1,  0, s1},
+            //        {  cy*s1,  0, c1},
+            //    });
+            //    }
+            //    else
+            //    {
+            //        gamma = Matrix<double>.Build.DenseOfArray(new double[,]
+            //    {
+            //        {                     cx,       cy,                   cz},
+            //        {(-cx*cy*c1 - cz*s1)/cxz,   cxz*c1,(-cy*cz*c1+cx*s1)/cxz},
+            //        {   (cx*cy*s1-cz*c1)/cxz,  -cxz*s1, (cy*cz*s1+cx*c1)/cxz},
+            //    });
+            //    }
 
-                double C1 = (G * J) / L;
+            //    var bd = Matrix<double>.Build;
 
-                Matrix<double> K_elem = DenseMatrix.OfArray(new double[,]
-                {
-                    { A1,    0,    0,    0,    0,    0,  -A1,    0,    0,    0,    0,    0 },
-                    {  0,  kz1,    0,    0,    0,  kz2,    0, -kz1,    0,    0,    0,  kz2 },
-                    {  0,    0,  ky1,    0, -ky2,    0,    0,    0, -ky1,    0, -ky2,    0 },
-                    {  0,    0,    0,   C1,    0,    0,    0,    0,    0,  -C1,    0,    0 },
-                    {  0,    0, -ky2,    0,  ky3,    0,    0,    0,  ky2,    0,  ky4,    0 },
-                    {  0,  kz2,    0,    0,    0,  kz3,    0, -kz2,    0,    0,    0,  kz4 },
-                    {-A1,    0,    0,    0,    0,    0,   A1,    0,    0,    0,    0,    0 },
-                    {  0, -kz1,    0,    0,    0, -kz2,    0,  kz1,    0,    0,    0, -kz2 },
-                    {  0,    0, -ky1,    0,  ky2,    0,    0,    0,  ky1,    0,  ky2,    0 },
-                    {  0,    0,    0,  -C1,    0,    0,    0,    0,    0,   C1,    0,    0 },
-                    {  0,    0, -ky2,    0,  ky4,    0,    0,    0,  ky2,    0,  ky3,    0 },
-                    {  0,  kz2,    0,    0,    0,  kz4,    0, -kz2,    0,    0,    0,  kz3 },
-                });
+            //    Matrix<double> T1;
+            //    T1 = gamma.Append(bd.Dense(3, 9));
+            //    Matrix<double> T2;
+            //    T2 = bd.Dense(3, 3).Append(gamma);
+            //    T2 = T2.Append(bd.Dense(3, 6));
+            //    Matrix<double> T3;
+            //    T3 = bd.Dense(3, 6).Append(gamma);
+            //    T3 = T3.Append(bd.Dense(3, 3));
+            //    Matrix<double> T4;
+            //    T4 = bd.Dense(3, 9).Append(gamma);
+            //    Matrix<double> T;
+            //    T = T1.Stack(T2);
+            //    T = T.Stack(T3);
+            //    T = T.Stack(T4);
 
-                K_elem = K_elem.Multiply(T);
-                K_elem = T_T.Multiply(K_elem);
+            //    //Matrix<double> T = SparseMatrix.OfArray(new double[,]
+            //    //{
+            //    //    { cx, cy, cz, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            //    //    { cx, cy, cz, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            //    //    { cx, cy, cz, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            //    //    { 0, 0, 0, cx, cy, cz, 0, 0, 0, 0, 0, 0 },
+            //    //    { 0, 0, 0, cx, cy, cz, 0, 0, 0, 0, 0, 0 },
+            //    //    { 0, 0, 0, cx, cy, cz, 0, 0, 0, 0, 0, 0 },
+            //    //    { 0, 0, 0, 0, 0, 0, cx, cy, cz, 0, 0, 0 },
+            //    //    { 0, 0, 0, 0, 0, 0, cx, cy, cz, 0, 0, 0 },
+            //    //    { 0, 0, 0, 0, 0, 0, cx, cy, cz, 0, 0, 0 },
+            //    //    { 0, 0, 0, 0, 0, 0, 0, 0, 0, cx, cy, cz },
+            //    //    { 0, 0, 0, 0, 0, 0, 0, 0, 0, cx, cy, cz },
+            //    //    { 0, 0, 0, 0, 0, 0, 0, 0, 0, cx, cy, cz },
+            //    //});
 
-                int node1 = points.IndexOf(p1);
-                int node2 = points.IndexOf(p2);
+            //    Matrix<double> T_T = T.Transpose();
 
-                //System.Diagnostics.Debug.WriteLine("Node1: " + node1.ToString() + ", Node2: " + node2.ToString());
+            //    double A1 = (E * A) / (L);
 
-                //PrintMatrix(K_elem,"K_elem");
+            //    double kz1 = (12 * E * Iz) / (L * L * L);
+            //    double kz2 = (6 * E * Iz) / (L * L);
+            //    double kz3 = (4 * E * Iz) / L;
+            //    double kz4 = (2 * E * Iz) / L;
 
-                //Inputting values to correct entries in Global Stiffness Matrix
-                for (int i = 0; i < K_elem.RowCount / 2; i++)
-                {
-                    //top left 3x3 of k-element matrix
-                    for (int j = 0; j < K_elem.ColumnCount / 2; j++)
-                    {
-                        K_tot[node1 * 6 + i, node1 * 6 + j] += K_elem[i, j];
-                    }
-                    //top right 3x3 of k-element matrix  
-                    for (int j = 0; j < K_elem.ColumnCount / 2; j++)
-                    {
-                        K_tot[node1 * 6 + i, node2 * 6 + j] += K_elem[i, j + 6];
-                    }
-                    //bottom left 3x3 of k-element matrix
-                    for (int j = 0; j < K_elem.ColumnCount / 2; j++)
-                    {
-                        K_tot[node2 * 6 + i, node1 * 6 + j] += K_elem[i + 6, j];
-                    }
-                    //bottom right 3x3 of k-element matrix
-                    for (int j = 0; j < K_elem.ColumnCount / 2; j++)
-                    {
-                        K_tot[node2 * 6 + i, node2 * 6 + j] += K_elem[i + 6, j + 6];
-                    }
-                }
-            }
+            //    double ky1 = (12 * E * Iy) / (L * L * L);
+            //    double ky2 = (6 * E * Iy) / (L * L);
+            //    double ky3 = (4 * E * Iy) / L;
+            //    double ky4 = (2 * E * Iy) / L;
+
+            //    double C1 = (G * J) / L;
+
+            //    Matrix<double> K_elem = DenseMatrix.OfArray(new double[,]
+            //    {
+            //        { A1,    0,    0,    0,    0,    0,  -A1,    0,    0,    0,    0,    0 },
+            //        {  0,  kz1,    0,    0,    0,  kz2,    0, -kz1,    0,    0,    0,  kz2 },
+            //        {  0,    0,  ky1,    0, -ky2,    0,    0,    0, -ky1,    0, -ky2,    0 },
+            //        {  0,    0,    0,   C1,    0,    0,    0,    0,    0,  -C1,    0,    0 },
+            //        {  0,    0, -ky2,    0,  ky3,    0,    0,    0,  ky2,    0,  ky4,    0 },
+            //        {  0,  kz2,    0,    0,    0,  kz3,    0, -kz2,    0,    0,    0,  kz4 },
+            //        {-A1,    0,    0,    0,    0,    0,   A1,    0,    0,    0,    0,    0 },
+            //        {  0, -kz1,    0,    0,    0, -kz2,    0,  kz1,    0,    0,    0, -kz2 },
+            //        {  0,    0, -ky1,    0,  ky2,    0,    0,    0,  ky1,    0,  ky2,    0 },
+            //        {  0,    0,    0,  -C1,    0,    0,    0,    0,    0,   C1,    0,    0 },
+            //        {  0,    0, -ky2,    0,  ky4,    0,    0,    0,  ky2,    0,  ky3,    0 },
+            //        {  0,  kz2,    0,    0,    0,  kz4,    0, -kz2,    0,    0,    0,  kz3 },
+            //    });
+
+            //    K_elem = K_elem.Multiply(T);
+            //    K_elem = T_T.Multiply(K_elem);
+
+            //    int node1 = points.IndexOf(p1);
+            //    int node2 = points.IndexOf(p2);
+
+            //    //System.Diagnostics.Debug.WriteLine("Node1: " + node1.ToString() + ", Node2: " + node2.ToString());
+
+            //    //PrintMatrix(K_elem,"K_elem");
+
+            //    //Inputting values to correct entries in Global Stiffness Matrix
+            //    for (int i = 0; i < K_elem.RowCount / 2; i++)
+            //    {
+            //        //top left 3x3 of k-element matrix
+            //        for (int j = 0; j < K_elem.ColumnCount / 2; j++)
+            //        {
+            //            K_tot[node1 * 6 + i, node1 * 6 + j] += K_elem[i, j];
+            //        }
+            //        //top right 3x3 of k-element matrix  
+            //        for (int j = 0; j < K_elem.ColumnCount / 2; j++)
+            //        {
+            //            K_tot[node1 * 6 + i, node2 * 6 + j] += K_elem[i, j + 6];
+            //        }
+            //        //bottom left 3x3 of k-element matrix
+            //        for (int j = 0; j < K_elem.ColumnCount / 2; j++)
+            //        {
+            //            K_tot[node2 * 6 + i, node1 * 6 + j] += K_elem[i + 6, j];
+            //        }
+            //        //bottom right 3x3 of k-element matrix
+            //        for (int j = 0; j < K_elem.ColumnCount / 2; j++)
+            //        {
+            //            K_tot[node2 * 6 + i, node2 * 6 + j] += K_elem[i + 6, j + 6];
+            //        }
+            //    }
+            //}
+            #endregion
+
             return K_tot;
         }
 
